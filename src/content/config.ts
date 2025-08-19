bcfceb3d6ddd18764e8a8220a4d2f25ec9af58e0
ng <dev.linjie@gmail.com>
2025-08-19 16:50:35 +0800

    cate

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,7 +4,7 @@ const posts = defineCollection({
   schema: z.object({
     title: z.string(),
     date: z.coerce.date(),  // 更安全的日期转换
-    draft: z.boolean().optional()
+    category: z.string().optional() 
   })
 });
 
