b74cc1f5f7fff843ee13352ec7490dc30c0df163
ng <dev.linjie@gmail.com>
2025-08-19 13:39:00 +0800

    init

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://zailaib.github.io',
+  base: '/', // 如果是项目页（非用户页），改为 '/repo-name/'
+});
+
---
+++ src/src/pages/fist-post.md
@@ -0,0 +1,10 @@
+---
+title: 我的第一篇文章
+date: 2023-10-01
+layout: ../../layouts/PostLayout.astro
+
+---
+
+# 欢迎阅读！
+
+这是用 **Markdown** 写的内容，Astro 会自动解析。
